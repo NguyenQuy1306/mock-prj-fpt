2ff5a1041c4b63a66ccbe882490a8168fd2eafdb
thái giản đình <giandinhthai@gmail.com>
2024-07-02 10:29:52 +0700

    merge init

--- src/main/java/com/curcus/lms/model/mapper/CourseMapper.java
+++ src/main/java/com/curcus/lms/model/mapper/CourseMapper.java
@@ -11,11 +11,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface CourseMapper {
 
-    @Mapping(source = "instructor.userId", target = "instructorId")
-    @Mapping(source = "instructor", target = "instructor")
-    @Mapping(source = "category.categoryId", target = "categoryId")
-    @Mapping(source = "category", target = "category")
-    @Mapping(source = "enrollment", target = "enrollment")
+
     CourseResponse toResponse(Course course);
 
     List<CourseResponse> toResponseList(List<Course> courses);
