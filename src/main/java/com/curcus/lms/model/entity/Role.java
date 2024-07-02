b42364be55d2892ef4a6a4b9a465735de39dc61d
MẪN HỒNG HUY <man.hong9102@hcmut.edu.vn>
2024-07-02 13:51:44 +0700

    feat: add Permission and Role entities

---
+++ src/main/java/com/curcus/lms/model/entity/Role.java
@@ -0,0 +1,69 @@
+package com.curcus.lms.model.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.curcus.lms.model.entity.Permission.ADMIN_CREATE;
+import static com.curcus.lms.model.entity.Permission.ADMIN_DELETE;
+import static com.curcus.lms.model.entity.Permission.ADMIN_READ;
+import static com.curcus.lms.model.entity.Permission.ADMIN_UPDATE;
+import static com.curcus.lms.model.entity.Permission.STUDENT_CREATE;
+import static com.curcus.lms.model.entity.Permission.STUDENT_DELETE;
+import static com.curcus.lms.model.entity.Permission.STUDENT_READ;
+import static com.curcus.lms.model.entity.Permission.STUDENT_UPDATE;
+import static com.curcus.lms.model.entity.Permission.INSTRUCTOR_CREATE;
+import static com.curcus.lms.model.entity.Permission.INSTRUCTOR_DELETE;
+import static com.curcus.lms.model.entity.Permission.INSTRUCTOR_READ;
+import static com.curcus.lms.model.entity.Permission.INSTRUCTOR_UPDATE;
+
+@RequiredArgsConstructor
+public enum Role {
+
+    USER(Collections.emptySet()),
+    ADMIN(Set.of(
+            ADMIN_CREATE,
+            ADMIN_READ,
+            ADMIN_UPDATE,
+            ADMIN_DELETE,
+            STUDENT_CREATE,
+            STUDENT_READ,
+            STUDENT_UPDATE,
+            STUDENT_DELETE,
+            INSTRUCTOR_CREATE,
+            INSTRUCTOR_READ,
+            INSTRUCTOR_UPDATE,
+            INSTRUCTOR_DELETE
+    )),
+    STUDENT(Set.of(
+            STUDENT_CREATE,
+            STUDENT_READ,
+            STUDENT_UPDATE,
+            STUDENT_DELETE
+    )),
+    INSTRUCTOR(Set.of(
+            INSTRUCTOR_CREATE,
+            INSTRUCTOR_READ,
+            INSTRUCTOR_UPDATE,
+            INSTRUCTOR_DELETE
+    ))
+
+    ;
+
+    @Getter
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    }
+}
