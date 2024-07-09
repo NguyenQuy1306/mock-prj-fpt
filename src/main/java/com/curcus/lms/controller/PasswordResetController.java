6cc79c054a5e5957fa0dcaef3facb1656b325c77
MẪN HỒNG HUY <man.hong9102@hcmut.edu.vn>
2024-07-09 15:16:40 +0700

    feat: add Reset Password

---
+++ src/main/java/com/curcus/lms/controller/PasswordResetController.java
@@ -0,0 +1,40 @@
+package com.curcus.lms.controller;
+
+import com.curcus.lms.model.request.EmailRequest;
+import com.curcus.lms.service.impl.PasswordResetImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequestMapping("/password-reset")
+public class PasswordResetController {
+    @Autowired
+    PasswordResetImpl passwordReset;
+    @PostMapping("/request")
+    public ResponseEntity<Boolean> request(@RequestBody EmailRequest emailRequest) {
+        return ResponseEntity.ok(passwordReset.requestPasswordReset(emailRequest.getEmail()));
+    }
+
+    @GetMapping("/reset")
+    public RedirectView reset(@RequestParam String token) {
+        Boolean result = passwordReset.resetPassword(token);
+        if (result) {
+            return new RedirectView("http://localhost:8080/password-reset/successful");
+        } else {
+            return new RedirectView("http://localhost:8080/password-reset/failed");
+        }
+    }
+
+    @GetMapping("/successful")
+    public ResponseEntity<String> successful() {
+        return ResponseEntity.ok("Reset mật khẩu thành công. Một email chứa mật khẩu mới đã được gửi vào email của bạn.");
+    }
+
+    @GetMapping("/failed")
+    public ResponseEntity<String> unsuccessful() {
+        return ResponseEntity.ok("Đã xảy ra lỗi");
+    }
+}
--- src/main/java/com/curcus/lms/service/VerificationTokenService.java
+++ src/main/java/com/curcus/lms/service/VerificationTokenService.java
@@ -1,7 +1,10 @@
 package com.curcus.lms.service;
 
+import com.curcus.lms.model.entity.User;
+
 import java.util.Optional;
 
 public interface VerificationTokenService {
     Optional<String> createVerificationToken(String email);
+    Optional<User> validateVerificationToken(String token);
 }
