5f0291ce7329b8ad2495bc8edef996cd083bcfe4
NyattoMin <dinhnguyen11050@gmail.com>
2024-07-11 16:09:55 +0700

    Add: getListCourseFromCart and Update: codebase

--- src/main/java/com/curcus/lms/controller/CartController.java
+++ src/main/java/com/curcus/lms/controller/CartController.java
@@ -7,28 +7,15 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.curcus.lms.exception.ApplicationException;
-import com.curcus.lms.exception.NotFoundException;
 import com.curcus.lms.exception.ValidationException;
-import com.curcus.lms.model.entity.CartItems;
-import com.curcus.lms.model.request.CourseRequest;
 import com.curcus.lms.model.response.ApiResponse;
 import com.curcus.lms.model.response.CourseResponse;
-import com.curcus.lms.model.response.StudentResponse;
 import com.curcus.lms.service.CartService;
-import com.curcus.lms.service.CourseService;
-import com.curcus.lms.service.StudentService;
-
-import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("api/cart")
