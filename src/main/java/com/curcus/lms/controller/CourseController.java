0713ec02e1103ef63c0d43a7a9065e4bb5adf6d7
Quý Nguyễn <quy.nguyennngocquy75@hcmut.edu.vn>
2024-07-13 03:37:51 +0000

    feat: update url of api updateCourse

--- src/main/java/com/curcus/lms/controller/CourseController.java
+++ src/main/java/com/curcus/lms/controller/CourseController.java
@@ -80,10 +80,14 @@ public class CourseController {
         }
     }
 
-    @PutMapping(value = "/updateCourse")
-    public ResponseEntity<ApiResponse<CourseResponse>> updateCourse(@Valid @RequestBody CourseRequest courseRequest,
+    @PutMapping(value = "/{course_id}/updateCourse")
+    public ResponseEntity<ApiResponse<CourseResponse>> updateCourse(@PathVariable Long course_id,
+            @Valid @RequestBody CourseRequest courseRequest,
             BindingResult bindingResult) {
         try {
+            if (course_id != courseRequest.getCourseId()) {
+                throw new ValidationException("CourseId for parameter of api updateCourse is wrong");
+            }
             CourseResponse courseResponse = courseService.update(courseRequest, bindingResult);
             ApiResponse apiResponse = new ApiResponse<>();
             apiResponse.ok(courseResponse);
@@ -92,10 +96,10 @@ public class CourseController {
             throw ex;
         } catch (ValidationException ex) {
             throw ex;
-            // other exception throw application() (not exception of user)
         } catch (Exception ex) {
             throw new ApplicationException();
         }
     }
+
 
 }
