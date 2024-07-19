01b4eb10f8ebdca4bb78c0be63914c8f6b42fddd
DrakkuN <dracnguyenbku@gmail.com>
2024-07-19 10:25:16 +0700

    fix web config

--- src/main/java/com/curcus/lms/config/WebConfig.java
+++ src/main/java/com/curcus/lms/config/WebConfig.java
@@ -1,3 +1,5 @@
+package com.curcus.lms.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
