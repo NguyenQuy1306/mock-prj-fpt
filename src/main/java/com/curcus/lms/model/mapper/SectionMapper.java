588188e6f03640e8f99e05ca0993e152f1773942
DrakkuN <dracnguyenbku@gmail.com>
2024-07-31 15:53:07 +0700

    fix sectioin id

--- src/main/java/com/curcus/lms/model/mapper/SectionMapper.java
+++ src/main/java/com/curcus/lms/model/mapper/SectionMapper.java
@@ -25,12 +25,12 @@ public abstract class SectionMapper {
 	@Mapping(source = "course.courseId", target = "courseId")
     public abstract SectionCreateResponse toResponse(Section section);
 
-    @Mapping(source = "sectionId", target = "sectionId")
+    @Mapping(source = "section.sectionId", target = "sectionId")
     @Mapping(source = "sectionName", target = "sectionName")
     @Mapping(source = "position", target = "position")
     public abstract SectionDetailResponse toDetailResponse(Section section);
 
-    @Mapping(source = "sectionId", target = "sectionId")
+    @Mapping(source = "section.sectionId", target = "sectionId")
     @Mapping(source = "sectionName", target = "sectionName")
     @Mapping(source = "position", target = "position")
     @Mapping(target = "contents", expression = "java(mapSortedContents(section))")
